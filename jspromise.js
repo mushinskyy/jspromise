1388251de6dce7c66eaad294d41a5381cc6a1704
Yaron Mushinsky <mushinskyy@icloud.com>
2017-06-06 15:03:35 +0300

    Completed jspromise.js

--- jspromise.js
+++ jspromise.js
@@ -1,6 +1,7 @@
 var PROMISE_PENDING = 0;
 var PROMISE_RESOLVED = 1;
 var PROMISE_REJECTED = 2;
+var PROMISE_COMPLETED = 2;
 
 function Promise() {
 	var state = PROMISE_PENDING;
@@ -8,16 +9,47 @@ function Promise() {
 	this.callbacks = new Array();
 
 	this.then = function(onResolve, onReject) {
-					var callback = { 
-						"onResolve": onResolve, 
-						"onReject" onReject 
-					};
+			var callback = { 
+				"onResolve": onResolve, 
+				"onReject" onReject 
+			};
 
-					this.callbacks.push(callback);
-				}
+			this.callbacks.push(callback);
+		}
 
-	this.resolve = function(value) { this.complete(PROMISE_RESOLVED, value) }
-	this.reject = function(value) { this.complete(PROMISE_REJECTED, value) }
+	this.resolve = function(value) { 
+		state = PROMISE_RESOLVED;
+		this.complete(value) 
+	}
 
-	this.complete = function(result, value) { }
+	this.reject = function(value) {
+		state = PROMISE_PENDING; 
+		this.complete(value) 
+	}
+
+	this.complete = function(value) {
+		if (result == PROMISE_PENDING)
+			throw new Error('Promise was not resolved nor rejected. ');
+			return;
+		}
+		if (result == PROMISE_COMPLETED)
+			throw new Error('Promise was already completed. ');
+			return;
+		}
+
+		this.callbacks.forEach( function(callback) {
+			if (result == PROMISE_RESOLVED) {
+				callback['onResolve'](value);
+			}
+			else if (result == PROMISE_REJECTED) {
+				callback['onReject'](value);
+			}
+		});
+
+		this.resolve = this.reject = function() {
+			throw new Error('Promise was already completed. ');
+		}
+
+		this.state = PROMISE_COMPLETED;
+	}
 }
